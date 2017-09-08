2998322de6696dd8a6c483655653078c5e462591
mikeshiwenbo <1281867218@qq.com>
2017-09-08 15:05:03 +0800

    完成初始下线功能

--- app/src/main/java/com/example/pc/broadcastbestpractice/BaseActivity.java
+++ app/src/main/java/com/example/pc/broadcastbestpractice/BaseActivity.java
@@ -1,8 +1,14 @@
 package com.example.pc.broadcastbestpractice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -10,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class BaseActivity extends AppCompatActivity {
+    private ForceOfflineReceiver receiver;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
@@ -17,8 +24,48 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter=new IntentFilter();
+        intentFilter.addAction("com.example.broadcastbestpractice.FORCE_OFFLINE");
+        receiver=new ForceOfflineReceiver();
+        registerReceiver(receiver,intentFilter);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(receiver!=null){
+            unregisterReceiver(receiver);
+            receiver=null;
+        }
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         ActivityCollector.removeActivity(this);
     }
+    class ForceOfflineReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+            AlertDialog.Builder builder=new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setMessage("You are forced to be offline." +
+                    "Please try to login again.");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCollector.finishAll();
+                    Intent intent1=new Intent(context,LoginActivity.class);
+                    context.startActivity(intent);
+                }
+            });
+            builder.show();
+        }
+    }
+
 }
+
--- app/src/main/java/com/example/pc/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/com/example/pc/broadcastbestpractice/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.example.pc.broadcastbestpractice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by pc on 2017/9/7.
@@ -27,6 +29,15 @@ public class LoginActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 String account=accountEdit.getText().toString();
+                String password=passwordEdit.getText().toString();
+                if(account.equals("admin")&&password.equals("123456")){
+                    Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Toast.makeText(LoginActivity.this,"account or password is invalid",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
